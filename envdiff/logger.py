df90bd81a9a28211fb7f30834eea634d02e3937d
Spencer Newton <sn.newton92@gmail.com>
2021-04-25 18:40:36 +0100

    refactored result table layout

--- envdiff/logger.py
+++ envdiff/logger.py
@@ -1,5 +1,6 @@
 from rich import print
 from rich.console import Console
+from rich.columns import Columns
 from rich.table import Table
 from rich.style import Style
 from rich import box
@@ -39,13 +40,18 @@ class Logger():
         """
         # shared = { 'key': { 'left_value', 'right_value' }, ... }
         # unique = { 'left': { 'key': 'value', ... }, 'right': { 'key': 'value', ...} }
-        self.console.print()
-        self.print_shared_table(shared, left_filename, right_filename)
-        self.console.print()
-        self.print_unique_table(unique, left_filename, right_filename)
-        self.console.print()
+        shared = self.get_shared_table(shared, left_filename, right_filename)
+        unique_left, unique_right = self.get_unique_tables(unique, left_filename, right_filename)
 
-    def print_shared_table(self, shared, left_filename, right_filename):
+        columns = Columns([unique_left, unique_right], expand=True)
+
+        # Do the printing
+        self.console.print()
+        self.console.print(shared)
+        self.console.print()
+        self.console.print(columns)
+
+    def get_shared_table(self, shared, left_filename, right_filename):
         """
         Log the table displaying the shared environment variable differences
         :shared, The dictionary of shared environment variables
@@ -61,29 +67,32 @@ class Logger():
         for key in dict(sorted(shared.items())):
             table.add_row(key, shared[key]['left'], shared[key]['right'])
 
-        self.console.print(table)
+        return table
 
-    def print_unique_table(self, unique, left_filename, right_filename):
+    def get_unique_tables(self, unique, left_filename, right_filename):
         """
         Log the table displaying the shared environment variable differences
         :unique, The dictionary of unique environment variables
         :left_filename, The filename of the first file
         :right_filename, The filename of the second file
         """
-        table = Table(title='Unique Environment Variables', box=box.ROUNDED, header_style=self.header_style, expand=True)
-
-        table.add_column('Variable', justify='right')
-        table.add_column('Value', style='red')
-
         unique_left = sorted(unique['left'].items())
         unique_right = sorted(unique['right'].items())
 
-        for k, v in unique_left[:-1]:
-            table.add_row(k, v, style=self.left_style)
+        # Create table for left file
+        left_table = Table(title=f'Unique {left_filename} Environment Variables', box=box.ROUNDED, header_style=self.header_style, expand=True)
+        left_table.add_column('Variable', justify='right')
+        left_table.add_column('Value', style='red')
 
-        table.add_row(unique_left[-1][0], unique_left[-1][1], style=self.left_style, end_section=True)
+        for k, v in unique_left:
+            left_table.add_row(k, v, style=self.left_style)
 
+        # Create table for right file
+        right_table = Table(title=f'Unique {right_filename} Environment Variables', box=box.ROUNDED, header_style=self.header_style, expand=True)
+        right_table.add_column('Variable', justify='right')
+        right_table.add_column('Value', style='red')
+        
         for k, v in unique_right:
-            table.add_row(k, v, style=self.right_style)
+            right_table.add_row(k, v, style=self.right_style)
 
-        self.console.print(table)
+        return left_table, right_table
