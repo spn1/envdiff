68a54c46b6fbf7d9a4f9d1124ca3a6f132ec5b3a
Spencer Newton <sn.newton92@gmail.com>
2021-05-01 19:47:44 +0100

    used argparser to better receive command line arguments

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
+import argparse
 from sys import argv
 from envdiff.diff import Diff
 from envdiff.logger import Logger
 
-if __name__ == '__main__':
-    try:
-        args = argv[1:3]
+description = """
+Processes two environment files to determine
+differences between the same keys
+"""
 
+def main():
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        '-c',
+        '--comment',
+        help='Define the characters used to begin a comment'
+        )
+    parser.add_argument(
+        '-s',
+        '--separator',
+        help='Define the characters used to separate the key-value pairs'
+        )
+    parser.add_argument('left', help='The filename of the first file')
+    parser.add_argument('right', help='The filename of the second file')
+
+    try:
+        # Get command-line arguments
+        args = vars(parser.parse_args())
+        left_file = args['left'];
+        right_file = args['right'];
+
+        # Initialise results logger
         logger = Logger()
         logger.print_title()
-        logger.print_args(args)
 
-        differ = Diff(left_file=args[0], right_file=args[1])
+        # Do the diffing
+        differ = Diff(left_file=left_file, right_file=right_file)
         shared, unique = differ.diff()
 
-        logger.print_diff(shared, unique, left_filename=args[0], right_filename=args[1])
+        # Print results
+        logger.print_diff(shared, unique, left_filename=left_file, right_filename=right_file)
     except IndexError:
         raise SystemExit(f'Usage: {argv[0]} <file> <file>')
     except FileNotFoundError:
         raise SystemExit(f'File not found')
+
+if __name__ == '__main__':
+    main()
